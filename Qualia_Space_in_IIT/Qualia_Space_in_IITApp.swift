9dd7e0b42ff8371676e12377d1471b3749c7fad6
Cong Le <longchike@gmail.com>
2025-06-30 01:27:07 -0700

    The setup

---
+++ Qualia_Space_in_IIT/Qualia_Space_in_IITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qualia_Space_in_IITApp.swift
+//  Qualia_Space_in_IIT
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Qualia_Space_in_IITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
